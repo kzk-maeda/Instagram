ee4ea5ab3758eca0f4892093a91101a7319879b6
kzk_maeda <casino.music0711@gmail.com>
2018-01-14 23:21:12 +0900

    first

---
+++ Instagram/Post Screen/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Kazuki Maeda on 2018/01/14.
+//  Copyright © 2018年 Kazuki Maeda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
+extension ViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) 
+        
+        return cell
+    }
+}
